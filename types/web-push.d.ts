d17415b0ed986d60e83a93d389e0caa1a5d3d186
Lukasz Borkowski <luki.borkowski@wp.pl>
2026-03-05 13:41:33 +0100

    notifi update

---
+++ types/web-push.d.ts
@@ -0,0 +1,5 @@
+declare module "web-push" {
+  export function setVapidDetails(subject: string, publicKey: string, privateKey: string): void;
+  export function sendNotification(subscription: any, payload: string): Promise<any>;
+  export function generateVAPIDKeys(): { publicKey: string; privateKey: string };
+}
